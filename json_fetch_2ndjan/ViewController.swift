dd306084214b8b749d4976d25693052f886faa23
Komal Barde <komal.vibeosys@gmail.com>
2020-01-02 11:25:27 +0530

    Initial Commit

---
+++ json_fetch_2ndjan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  json_fetch_2ndjan
+//
+//  Created by Komal Barde on 02/01/2020.
+//  Copyright © 2020 Komal Barde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
